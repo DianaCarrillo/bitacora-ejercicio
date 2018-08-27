6e03c165d4ab0b1c7b9161520ab2d1af9d633962
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-27 17:27:54 -0500

    subiendo lo último de la bitácora

--- js/app.js
+++ js/app.js
@@ -4,10 +4,14 @@ let profileContainer = document.getElementById('profile-container');
 let imgPhoto = document.getElementById('img-photo');
 // para file se utiliza el evento 'change'
 fileId.addEventListener('change', () =>{
+    imgPhoto.src = URL.createObjectURL(event.target.files[0]);
+    firebase.database().ref('users').set({
+        // username: user.displayName,
+        profPicture: imgPhoto.src
     // quiero pintar el src de la etiqueta img que está en HTML.  Utilizo URL.createObjectoURL El nuevo objeto URL representa al objeto File especificado 
-imgPhoto.src = URL.createObjectURL(event.target.files[0]);
 
 })
+});
 
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
@@ -21,7 +25,7 @@ firebase.auth().onAuthStateChanged(user => {
   const mandarUsuarioDatabase = (user) => {
     firebase.database().ref('users').set({
       username: user.displayName,
-      profPicture: imgPhoto
+      profPicture: imgPhoto.src
     });
     pintarUsuario(user);
   };
@@ -32,8 +36,10 @@ firebase.auth().onAuthStateChanged(user => {
       `<div class="container">
         <div class="row">
         <p class="name"> Hello  ${user.displayName}</p>
-        <div> ${imgPhoto.src}</div>        
+        <img src =" ${imgPhoto.src}"/>        
          </div>
          </div>`;
    
   };
+
+  // this is a comment to update my repo
